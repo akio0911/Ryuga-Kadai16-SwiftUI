46e78223740483dd327ee71ff173bd29c00497c4
Ryuga <ribereo.minami@gmail.com>
2023-04-14 21:09:41 +0900

    課題15を追加

---
+++ Kadai16-SwiftUI/AddItemView.swift
@@ -0,0 +1,65 @@
+//
+//  AddItemView.swift
+//  Kadai16-SwiftUI
+//
+//  Created by Ryuga on 2023/04/14.
+//
+
+import SwiftUI
+
+struct AddItemView: View {
+    @State private var newItem = ""
+    let didSave: (Fruit) -> Void
+    let didCancel: () -> Void
+
+    var body: some View {
+        NavigationStack{
+            VStack{
+                HStack {
+                    Text("名前")
+                    TextField("", text: $newItem)
+                        .modifier(CustomTextFieldStyle())
+                }
+                Spacer()
+            }
+            .listStyle(.plain)
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .cancellationAction) {
+                    Button("Cancel") {
+                        didCancel()
+                    }
+                }
+            }
+            .toolbar {
+                ToolbarItem(placement: .confirmationAction) {
+                    Button("Save") {
+                        didSave(Fruit(name: newItem, isChecked: false))
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct CustomTextFieldStyle: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .frame(width: 200, alignment: .trailing)
+            .textFieldStyle(RoundedBorderTextFieldStyle())
+    }
+}
+
+struct Preview: View {
+    @State var fruits = [Fruit(name: "りんご", isChecked: false)]
+    var body: some View {
+        AddItemView(didSave: { _ in }, didCancel: {})
+    }
+}
+
+struct AddItemView_Previews: PreviewProvider {
+    static var previews: some View {
+        Preview()
+    }
+}
+
--- Kadai16-SwiftUI/ContentView.swift
+++ Kadai16-SwiftUI/ContentView.swift
@@ -7,15 +7,66 @@
 
 import SwiftUI
 
+import SwiftUI
+
+struct Fruit: Identifiable {
+    let id = UUID()
+    var name: String
+    var isChecked: Bool
+}
+
 struct ContentView: View {
+    @State private var showingModal = false
+    @State var fruits = [
+        Fruit(name: "りんご", isChecked: false),
+        Fruit(name: "みかん", isChecked: true),
+        Fruit(name: "バナナ", isChecked: false),
+        Fruit(name: "パイナップル", isChecked: true)
+    ]
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            List {
+                ForEach(fruits.indices, id: \.self) { index in
+                    let fruit = fruits[index]
+                    ZStack {
+                        HStack{
+                            Image(systemName: "checkmark")
+                                .foregroundColor(fruit.isChecked ? Color.orange : Color.white)
+                            Spacer().frame(width: 15)
+                            Text(fruit.name)
+                            Spacer()
+                        }
+                        .contentShape(Rectangle())
+                        .onTapGesture {
+                            fruits[index].isChecked.toggle()
+                        }
+                    }
+                }
+            }
+            .listStyle(.plain)
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button(action: {
+                        showingModal = true
+                    }) {
+                        Image(systemName: "plus")
+                    }
+                    .fullScreenCover(isPresented: $showingModal) {
+                        AddItemView(
+                            didSave: { fruit in
+                                fruits.append(fruit)
+                                showingModal = false
+                            },
+                            didCancel: {
+                                showingModal = false
+                            }
+                        )
+                    }
+                }
+            }
         }
-        .padding()
     }
 }
 
@@ -24,3 +75,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
